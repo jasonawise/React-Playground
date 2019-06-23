5123f6030026069d60cd50bad00c72ef40a86d4d
Jason Wise <jasonandrewwise@gmail.com>
2019-06-22 22:40:08 -0400

    Learning and understand state in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: 'React Playground', description: 'This is to learn and play around with React.' };
+  }
+
   render() {
+    const { title, description } = this.state;
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </div>
     );
   }
